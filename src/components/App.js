519b06b8892065843149a7e3f72756bde9207c0e
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-07-07 15:22:22 -0700

    about to integrate svg to react component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './display/test.css';
+import { ReactComponent as Icon } from '../assets/wireframetest-09.svg';
 
 export default function App() {
-  return <h1 className={styles.header}>Hello World</h1>;
+  return (
+    <>
+      <h1 className={styles.header}>Hello World</h1>
+      <Icon width={100} />
+    </>
+  );
 }
---
+++ src/components/pages/LandingPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function LandingPage() {
+  <div>
+  {/* // outer most container for upper landing page container */}
+  <main>
+  {/* portion for text the about me and call to action */}
+  <div>
+    {/* portion will have the oh hai to be displayed as a block */}
+    
+  </div>
+  <div>
+    {/* portion will have the left justified text intro  */}
+  </div>
+  </main>
+
+  </div>
+}
+
+export default LandingPage;
