55e6540864189a4b3c8f91671912fb6299d38c06
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-07-08 14:42:49 -0700

    work header made

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import TestSvg from '../assets/wireframetest-09.svg';
+// import TestSvg from '../assets/wireframetest-09.svg';
 import './display/base.css';
 import LandingPage from '../components/pages/LandingPage';
 import Header from './pages/Header';
+import WorkHeader from './pages/WorkHeader';
 
 export default function App() {
   return (
     <>
       <Header />
       <LandingPage />
+      <WorkHeader />
       {/* <div>
         <TestSvg width={220} height={500}/>
       </div> */}
---
+++ src/components/pages/WorkHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../display/header.css';
+
+function WorkHeader() {
+  return (
+    <header className={styles.work}>
+      <div>
+        <h1>Wo</h1>
+      </div>
+      <div>
+        <h1>rk</h1>
+      </div>
+    </header>
+  );
+}
+
+export default WorkHeader;
