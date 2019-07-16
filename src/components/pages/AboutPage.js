95fbf06ecad60427c1143d4c46b1745aaf128211
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-07-16 13:28:31 -0700

    adding copy

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -6,16 +6,37 @@ function AboutPage() {
     <section className={styles.aboutContainer}>
       <div className={styles.main}>
         <h2>Bio</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Originally hailing from Chicago, I've spent the last few years executing projects for clients ranging from apparel to augmented reality. My passion for programming was born from a desire to not only continue to grow my skillset as a designer but to also transition into a field where I felt I could use my background to solve human related problems through tech. When I'm not tapping away at the computer, one can generally find me taking a bike ride, reading a book, or sketching in a coffee shop.</p>
       </div>
       <div className={styles.sideCol}>
         <div className={styles.side}>
           <h2>tech</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p>
+            - React/Redux <br />
+            - Javascript <br />
+            - HTML/CSS/PostCSS <br />
+            - Firebase <br />
+            - Node.js/Webpack <br />
+            - API Integration <br />
+            - Express.js/MongoDB <br />
+            - Jest/Enzyme/QUnit <br />
+          </p>
         </div>
         <div className={styles.side}>
           <h2>design</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p>
+            - UX/UI Development <br />
+            - Wireframing/ Prototyping <br />
+            - Accessibility <br />
+            - Logo/Brand Development<br />
+            - Digital/Visual/Print <br />
+            - Art Direction <br />
+            - Illustration <br />
+            - Marketing <br />
+            - Interactive Design <br />
+            - Adobe Suite <br />
+            - Adobe Suite <br />
+          </p>
         </div>
       </div>
     </section>
--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -11,11 +11,12 @@ function LandingPage() {
         <div className={styles.about}>
           {/* portion will have the left justified text intro  */}
           <p>
+            My name is Colin Coutts, <br />
             I'm a Software Developer <br />
             passionate about telling <br />
-            user stories and solving <br /> 
-            human-centered problems <br />
-            through code
+            user stories and collaborating <br /> 
+            with other coders to bring<br />
+            projects to life.
           </p>
         </div>
         {/* portion for text the about me and call to action */}
